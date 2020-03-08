3fdec479fdedc9617522f4c1048afa7a4d50ce3b
Carina Lewandowski <carinal@princeton.edu>
2020-03-08 15:57:42 -0400

    first test

--- mycospath.py
+++ mycospath.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return 'Welcome to myCOSPath.'
+
+# from https://stackabuse.com/deploying-a-flask-application-to-heroku/
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run()
