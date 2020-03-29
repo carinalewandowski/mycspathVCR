178fc7f6672d776df6e5cbeaaf0bbe2933fb8f4e
Carina Lewandowski <carinal@princeton.edu>
2020-03-28 22:03:07 -0400

    bootstrap + page navigation

--- mycspath.py
+++ mycspath.py
@@ -1,9 +1,36 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, make_response, redirect, url_for
+from flask import render_template, session
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
-def welcome():
-    return 'Welcome to myCOSPath.'
+def landing():
+    html = render_template('index.html')
+    response = make_response(html)
+    return response
+
+@app.route('/home')
+def home():
+    html = render_template('home.html')
+    response = make_response(html)
+    return response
+
+@app.route('/about')
+def about():
+    html = render_template('about.html')
+    response = make_response(html)
+    return response
+
+@app.route('/saved')
+def saved():
+    html = render_template('saved.html')
+    response = make_response(html)
+    return response
+
+@app.route('/profile')
+def profiles():
+    html = render_template('profile.html')
+    response = make_response(html)
+    return response
 
 # from https://stackabuse.com/deploying-a-flask-application-to-heroku/
 if __name__ == '__main__':
